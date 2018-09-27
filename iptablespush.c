33c7127f6682ea6ab7d84dbddc0321ad8995fffa
Gandalf <zhibu1991@gmail.com>
2018-09-27 18:26:10 +0800

    Add files via upload
    Increase rules repeated detection

--- iptablespush.c
+++ iptablespush.c
@@ -8,7 +8,46 @@
 #include <ctype.h>
 #include <string.h>
 #include <unistd.h>
+int execute_fork()
+{
+        fflush(stdout);
+        fflush(stderr);
+        return fork();
+}
 
+int  execute_popen(pid_t *pid, const char *command)
+{
+        int             fd[2];
+
+        if (-1 == pipe(fd))
+                return -1;
+
+        if (-1 == (*pid = execute_fork()))
+        {
+                close(fd[0]);
+                close(fd[1]);
+                return -1;
+        }
+
+        if (0 != *pid)  /* parent process */
+        {
+                close(fd[1]);
+                return fd[0];
+        }
+
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        dup2(fd[1], STDERR_FILENO);
+        close(fd[1]);
+        if (-1 == setpgid(0, 0))
+        {
+                exit(EXIT_SUCCESS);
+        }
+
+        execl("/bin/sh", "sh", "-c", command, NULL);
+
+        exit(EXIT_SUCCESS);
+}
 
 int IptablesPush_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
 {
@@ -18,17 +57,19 @@ int IptablesPush_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int
 	RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1],
 	REDISMODULE_READ | REDISMODULE_WRITE);
 	pid_t pid;
+	int fd;
+	char tmp_buf[4096];
 
-	static char command[256];
-	sprintf(command, "iptables -I INPUT -s %s -j DROP",
+	static char check_command[256], insert_command[256];
+	sprintf(check_command, "iptables -C INPUT -s %s -j DROP",
+			RedisModule_StringPtrLen(argv[1], NULL));
+	sprintf(insert_command, "iptables -I INPUT -s %s -j DROP",
 			RedisModule_StringPtrLen(argv[1], NULL));
 	printf("%s || %s\n", RedisModule_StringPtrLen(argv[1], NULL),
 			RedisModule_StringPtrLen(argv[2], NULL));
-	if ((pid = fork()) < 0) {
-		return -1;
-	} else if (pid == 0) {
-		execl("/bin/sh", "sh", "-c", command, NULL);
-		return -1;
+	fd = execute_popen(&pid, check_command);
+	if (0 < read(fd, tmp_buf, sizeof(tmp_buf) - 1)) {
+		execute_popen(&pid, insert_command);
 	}
 
 	RedisModule_StringSet(key, argv[2]);
