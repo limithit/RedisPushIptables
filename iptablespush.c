445845238e2d13aa94ca90a3becf100a3ff32615
Gandalf <zhibu1991@gmail.com>
2018-10-16 12:03:39 +0800

    Add files via upload

--- iptablespush.c
+++ iptablespush.c
@@ -8,45 +8,65 @@
 #include <ctype.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int redis_waitpid(pid_t pid)
+{
+	int             rc, status;
+	do
+	{
+		if (-1 == (rc = waitpid(pid, &status, WUNTRACED)))
+		{
+			goto exit;
+		}
+	}
+	while (!WIFEXITED(status) && !WIFSIGNALED(status));
+exit:
+
+	return rc;
+}
+
 int execute_fork()
 {
-        fflush(stdout);
-        fflush(stderr);
-        return fork();
+	fflush(stdout);
+	fflush(stderr);
+	return fork();
 }
 
 int  execute_popen(pid_t *pid, const char *command)
 {
-        int             fd[2];
+	int             fd[2];
 
-        if (-1 == pipe(fd))
-                return -1;
+	if (-1 == pipe(fd))
+		return -1;
 
-        if (-1 == (*pid = execute_fork()))
-        {
-                close(fd[0]);
-                close(fd[1]);
-                return -1;
-        }
+	if (-1 == (*pid = execute_fork()))
+	{
+		close(fd[0]);
+		close(fd[1]);
+		return -1;
+	}
 
-        if (0 != *pid)  /* parent process */
-        {
-                close(fd[1]);
-                return fd[0];
-        }
+	if (0 != *pid)  /* parent process */
+	{
+		close(fd[1]);
+		return fd[0];
+	}
 
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        dup2(fd[1], STDERR_FILENO);
-        close(fd[1]);
-        if (-1 == setpgid(0, 0))
-        {
-                exit(EXIT_SUCCESS);
-        }
+	close(fd[0]);
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(fd[1], STDERR_FILENO);
+	close(fd[1]);
+	if (-1 == setpgid(0, 0))
+	{
+		exit(EXIT_SUCCESS);
+	}
 
-        execl("/bin/sh", "sh", "-c", command, NULL);
+	execl("/bin/sh", "sh", "-c", command, NULL);
 
-        exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
 
 int IptablesPush_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
@@ -55,7 +75,7 @@ int IptablesPush_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int
 		return RedisModule_WrongArity(ctx);
 
 	RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1],
-	REDISMODULE_READ | REDISMODULE_WRITE);
+			REDISMODULE_READ | REDISMODULE_WRITE);
 	pid_t pid;
 	int fd;
 	char tmp_buf[4096];
@@ -68,9 +88,13 @@ int IptablesPush_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int
 	printf("%s || %s\n", RedisModule_StringPtrLen(argv[1], NULL),
 			RedisModule_StringPtrLen(argv[2], NULL));
 	fd = execute_popen(&pid, check_command);
-	if (0 < read(fd, tmp_buf, sizeof(tmp_buf) - 1)) {
-		execute_popen(&pid, insert_command);
-	}
+	redis_waitpid(pid);
+		if (0 < read(fd, tmp_buf, sizeof(tmp_buf) - 1)) {
+			close(fd);
+			execute_popen(&pid, insert_command);
+			redis_waitpid(pid);
+		}
+	close(fd);
 
 	RedisModule_StringSet(key, argv[2]);
 	size_t newlen = RedisModule_ValueLength(key);
@@ -83,19 +107,19 @@ int IptablesPush_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int
 /* This function must be present on each Redis module. It is used in order to
  * register the commands into the Redis server. */
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-    if (RedisModule_Init(ctx,"iptables-insert",1,REDISMODULE_APIVER_1)
-        == REDISMODULE_ERR) return REDISMODULE_ERR;
+	if (RedisModule_Init(ctx,"iptables-insert",1,REDISMODULE_APIVER_1)
+			== REDISMODULE_ERR) return REDISMODULE_ERR;
 
-    /* Log the list of parameters passing loading the module. */
-    for (int j = 0; j < argc; j++) {
-        const char *s = RedisModule_StringPtrLen(argv[j],NULL);
-        printf("Module loaded with ARGV[%d] = %s\n", j, s);
-    }
+	/* Log the list of parameters passing loading the module. */
+	for (int j = 0; j < argc; j++) {
+		const char *s = RedisModule_StringPtrLen(argv[j],NULL);
+		printf("Module loaded with ARGV[%d] = %s\n", j, s);
+	}
 
-    if (RedisModule_CreateCommand(ctx,"iptables.push",
-    		IptablesPush_RedisCommand,"write deny-oom",1,1,1) == REDISMODULE_ERR)
-        return REDISMODULE_ERR;
+	if (RedisModule_CreateCommand(ctx,"iptables.push",
+				IptablesPush_RedisCommand,"write deny-oom",1,1,1) == REDISMODULE_ERR)
+		return REDISMODULE_ERR;
 
 
-    return REDISMODULE_OK;
+	return REDISMODULE_OK;
 }
