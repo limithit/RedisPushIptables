7c25550f4eaed38ff653446c5e417b4a55d42926
Gandalf <zhibu1991@gmail.com>
2019-03-15 19:23:34 +0800

    Update ttl_iptables.c

--- ttl_iptables.c
+++ ttl_iptables.c
@@ -87,7 +87,6 @@ int check_ipaddr(char *str) {
 }
 
 int main(int argc, char **argv) {
-	unsigned int j;
 	redisContext *c;
 	redisReply *reply;
 	const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
